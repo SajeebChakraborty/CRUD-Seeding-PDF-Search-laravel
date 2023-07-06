8929f36a51acb3419676fa85d3d57990df62ea8f
Sajeeb Chakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-07-06 17:23:00 +0600

    seeding

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+//model added
+use App\Models\Book;
+use Illuminate\Support\Str;
+use Nette\Utils\Random;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 100; $i++) {
+            Book::create([
+                'name' => Str::random(10),
+                'author' => Str::random(10),
+                'isbn' => random_int(10001, 999999),
+                'quantity' => random_int(10, 100),
+                'price' => random_int(1, 100)
+            ]);
+        }
+    }
+}
